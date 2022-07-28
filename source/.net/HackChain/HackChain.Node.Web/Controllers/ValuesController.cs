9cc84d8eb6d1dc57caf0f3564bdc3e5571c65dfa
pirin <pirin.karabenchev@gmail.com>
2022-07-28 17:38:00 +0300

    web project added for the Node

---
+++ source/.net/HackChain/HackChain.Node.Web/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using HackChain.Core.Model;
+using HackChain.Node.Web.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackChain.Node.Web.Controllers
+{
+    [Route("api/transactions")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        [Route("add")]
+        [HttpPost]
+        public ActionResult<TransactionDTO> AddTransaction([FromBody] TransactionDTO transaction)
+        {
+
+
+            return transaction;
+        }
+    }
+}
---
+++ source/.net/HackChain/HackChain.Node.Web/DTO/TransactionDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace HackChain.Node.Web.DTO
+{
+    public class TransactionDTO
+    {
+        public string Sender { get; set; }
+        public string Recipient { get; set; }
+        public string Nonce { get; set; }
+        public string? Data { get; set; }
+        public string Value { get; set; }
+        public string Fee { get; set; }
+        public string Hash { get; set; }
+        public string Signature { get; set; }
+    }
+}
