c09678c67cc634c29c5162630528a4b1017162d0
Petia Latinova <petia@hack.bg>
2022-08-04 22:54:24 +0300

    wip: add backend url and sanity checks

--- source/faucet/backend/src/models/address-schema.ts
+++ source/faucet/backend/src/models/address-schema.ts
@@ -2,14 +2,10 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-
 // TODO: 
 const addressSchema = new Schema({
-    address: String,
-    time: { 
-        type: String,
-        required: true
-    }
+  address: { type: String, required: true },
+  time: { type: String, required: true }
 });
 
 export const Address = mongoose.model('Story', addressSchema);
--- source/faucet/frontend/src/App.tsx
+++ source/faucet/frontend/src/App.tsx
@@ -7,8 +7,10 @@ function App() {
   const [hash, setHash] = useState('');
 
   const send = () => {
-    // TODO validations and sanity checks
-    axios.post('/send', {
+    if (!isAddress) {
+      return;
+    }
+    axios.post(`${process.env.BACKEND_URL}/send`, {
       address: address,
     })
     .then(function (response) {
@@ -21,9 +23,14 @@ function App() {
     console.log(address);
   }
 
+  const isAddress = /^[0-9a-fA-F]{130}/.test(address);
+
   return (
     <div className="App">
+      <h1>HACK ACKADEMY FAUCET</h1>
+      <h2>Fast and reliable. 0.5 TestHCT HCT/day</h2>
       <input type="text" id="address" placeholder='enter address' onChange={(event) => setAddress(event.target.value)}></input>
+      {!isAddress && <text id="error-address">Invalid address</text>}
       <button id="send" onClick={send}>Send</button>
       {hash && <p>Transaction URL: https://TODO_EXPLORER_URL/transaction/{hash}</p>}
     </div>
