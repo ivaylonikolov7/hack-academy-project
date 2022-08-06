7edd0f36750edde73f81ebf8e4be5dc0590d52be
pirin <pirin.karabenchev@gmail.com>
2022-08-06 16:30:07 +0300

    Known peer nodes urls added to settings

--- source/.net/HackChain/HackChain.Core/Model/HackChainSettings.cs
+++ source/.net/HackChain/HackChain.Core/Model/HackChainSettings.cs
@@ -4,6 +4,7 @@
     {
         public string NodeId { get; set; }
         public string BaseUrl { get; set; }
+        public IEnumerable<string> KnownPeerNodesBaseUrls { get; set; }
         public int Difficulty { get; set; }
         public long CoinbaseValue { get; set; }
         public string MinersPrivateKeyHex { get; set; }
