7975468eb24ec6ca502dbc3d11ebb2b9ba1fe215
pirin <pirin.karabenchev@gmail.com>
2022-08-16 11:59:04 +0300

    node dashboard

--- source/.net/HackChain/HackChain.Node.Web/Pages/Index.cshtml.cs
+++ source/.net/HackChain/HackChain.Node.Web/Pages/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HackChain.Core.Interfaces;
+using HackChain.Core.Model;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HackChain.Node.Web.Pages
@@ -6,15 +8,34 @@ namespace HackChain.Node.Web.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly INodeService _nodeService;
+        private readonly IAccountService _accountService;
+        private readonly ITransactionService _transactionService;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public NodeStatus Status { get; set; }
+        public IEnumerable<Block> Blocks { get; set; }
+        public IEnumerable<Account> Accounts { get; set; }
+        public IEnumerable<Transaction> PendingTransactions { get; set; }
+
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            INodeService nodeService,
+            IAccountService accountService,
+            ITransactionService transactionService
+            )
         {
             _logger = logger;
+            _nodeService = nodeService;
+            _accountService = accountService;
+            _transactionService = transactionService;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
-
+            Status = await _nodeService.GetNodeStatus();
+            Blocks = await _nodeService.GetLast(10);
+            Accounts = await _accountService.GetMostFunded(10);
+            PendingTransactions = await _transactionService.GetPendingTransactions();
         }
     }
 }
